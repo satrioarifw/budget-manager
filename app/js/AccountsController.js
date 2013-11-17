7ef97d43fd6071da4aa0fb583dcff93f031733c3
Kevin Delemme <kdelemme@gmail.com>
2013-11-17 13:18:23 +0100

    Adding List of Accounts and Multiple Account Handler

--- app/js/AccountsController.js
+++ app/js/AccountsController.js
@@ -1,66 +1,82 @@
-function AccountsCtrl($scope, $http) {
-	$http.get('mock/accounts.json').success(function(data) {
-      $scope.accounts = data;
-    });
+var budgetControllers = angular.module('budgetControllers', []);
 
-    $http.get('mock/categories.json').success(function(data) {
-      $scope.categories = data;
-    });
+budgetControllers.controller('AccountListCtrl', ['$scope', '$http',
+	function AccountListCtrl($scope, $http) {
+	    $http.get('mock/accounts.json').success(function(data) {
 
-    //init addRecordForm
-    var defaultAddRecordForm = {
-        amount: 0.0,
-        type: 0,
-        category: "Utilities",
-        description: ""
-    };
-    
-    $scope.record = defaultAddRecordForm
+	    	$scope.accounts = data;
+	    });
+	}]);
 
-    $scope.getBalance = function() {
-    	var records = $scope.accounts.records;
-    	var balance = 0.0;
-    	for (var recordKey in records) {
-    		if (records[recordKey].is_expense) {
-    			balance -= records[recordKey].amount;
-    		}
-    		else {
-    			balance += records[recordKey].amount;
-    		}
-    	}
-    	return balance;
-    };
+budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$http',
+	function AccountDetailCtrl($scope, $routeParams, $http) {
+		$http.get('mock/accounts.json').success(function(data) {
+			for (var accountKey in data) {
+				if (data[accountKey].id == $routeParams.accountId) {
+					$scope.account = data[accountKey];
+					return ;
+				}
+			}
+		});
 
-    $scope.addRecord = function(record) {
-    	var amount = Number(record.amount);
-    	if (isNaN(amount) || amount <= 0) {
-    		$scope.record = defaultAddRecordForm;
-    		return ;
-    	}
+		$http.get('mock/categories.json').success(function(data) {
+	    	$scope.categories = data;
+	    });
 
-    	$scope.addRecordForm.$setPristine();
+	    //init addRecordForm
+	    var defaultAddRecordForm = {
+	        amount: 0.0,
+	        type: 0,
+	        category: "Utilities",
+	        description: ""
+	    };
+	    
+	    $scope.record = defaultAddRecordForm
 
-    	//Create the new record with form input values
-    	var r 			= new Object();
-    	r.category 		= record.category;
-    	r.description 	= record.description;
-    	r.date 			= new Date().getTime();
-    	r.amount 		= amount;
-    	r.is_expense 	= record.type == 0 ? true : false;
+	    $scope.getBalance = function() {
+	    	var records = $scope.account.records;
+	    	var balance = 0.0;
+	    	for (var recordKey in records) {
+	    		if (records[recordKey].is_expense) {
+	    			balance -= records[recordKey].amount;
+	    		}
+	    		else {
+	    			balance += records[recordKey].amount;
+	    		}
+	    	}
+	    	return balance;
+	    };
 
-    	//Set form inputs with default values
-    	$scope.record = defaultAddRecordForm;
+	    $scope.addRecord = function(record) {
+	    	var amount = Number(record.amount);
+	    	if (isNaN(amount) || amount <= 0) {
+	    		$scope.record = defaultAddRecordForm;
+	    		return ;
+	    	}
 
-    	return $scope.accounts.records.push(r);
-    };
+	    	$scope.addRecordForm.$setPristine();
 
-    $scope.deleteRecord = function(record) {
-    	var records = $scope.accounts.records;
-    	for (var recordKey in records) {
-    		if (records[recordKey].id == record.id) {
-    			$scope.accounts.records.splice(recordKey, 1);
-    			return ;
-    		}
-    	}
-    }
-}
+	    	//Create the new record with form input values
+	    	var r 			= new Object();
+	    	r.category 		= record.category;
+	    	r.description 	= record.description;
+	    	r.date 			= new Date().getTime();
+	    	r.amount 		= amount;
+	    	r.is_expense 	= record.type == 0 ? true : false;
+
+	    	//Set form inputs with default values
+	    	$scope.record = defaultAddRecordForm;
+
+	    	return $scope.account.records.push(r);
+	    };
+
+	    $scope.deleteRecord = function(record) {
+	    	var records = $scope.account.records;
+	    	for (var recordKey in records) {
+	    		if (records[recordKey].id == record.id) {
+	    			$scope.account.records.splice(recordKey, 1);
+	    			return ;
+	    		}
+	    	}
+	    }
+	}]);
--- app/js/app.js
+++ app/js/app.js
@@ -2,4 +2,23 @@
 
 /* App Module */
 
-var budgetApp = angular.module('budgetApp', []);
+var budgetApp = angular.module('budgetApp', [
+  'ngRoute',
+  'budgetControllers'
+]);
+
+budgetApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/accounts', {
+        templateUrl: 'partials/account-list.html',
+        controller: 'AccountListCtrl'
+      }).
+      when('/accounts/:accountId', {
+        templateUrl: 'partials/account-detail.html',
+        controller: 'AccountDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/accounts'
+      });
+  }]);
