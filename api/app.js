6d35576b96e33d0886d0988a17745a8f7a9dbd27
Kevin Delemme <kdelemme@gmail.com>
2013-11-17 19:49:59 +0100

    Add: Create Account, Delete Account, Delete Record, Add Record - Persistence in memory in Node.js for now

--- api/app.js
+++ api/app.js
@@ -1,13 +1,25 @@
 var express = require('express');
 var app = express();
 
+app.use(express.bodyParser());
+
+
 var accounts = require('./mock/accounts.json');
 var categories = require('./mock/categories.json');
 var error = {"error": true, "message": "No data available."};
 
-app.get('/accounts', function(req, res) {
-	res.header('Access-Control-Allow-Origin', "http://localhost");
 
+app.all('*', function(req, res, next) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+  if ('OPTIONS' == req.method) return res.send(200);
+  next();
+});
+
+
+//Get all accounts and compute balance.
+app.get('/accounts', function(req, res) {
 	for (var accountKey in accounts) {
 		var balance = 0.0;
 		for (var recordKey in accounts[accountKey].records) {
@@ -24,21 +36,81 @@ app.get('/accounts', function(req, res) {
   	res.json(accounts);
 });
 
-app.get('/accounts/:id', function(req, res) {
-	res.header('Access-Control-Allow-Origin', "http://localhost");
+//Add new Account
+app.post('/accounts', function(req, res) {
+	var account 		= new Object();
+	account.name 		= req.body.name;
+	account.currency 	= req.body.currency;
+	account.id 			= Math.ceil(Math.random() * 1000000); //TODO Change when integrated with MongoDB
+	account.balance 	= 0;
+	account.records		= [];
 
+	accounts.push(account);
+	res.json(200, {id:account.id});
+});
+
+//Delete Account
+app.delete('/accounts/:id', function(req, res) {
+	for(var accountKey in accounts) {
+		if (accounts[accountKey].id == req.params.id) {
+			accounts.splice(accountKey, 1);
+			res.send(200);
+		}
+	}
+
+	res.json(404, error);
+});
+
+//Get specific account
+app.get('/accounts/:id', function(req, res) {
 	for(var accountKey in accounts) {
 		if (accounts[accountKey].id == req.params.id) {
 			res.json(accounts[accountKey]);
 		}
 	}
 
-	res.json(error);
+	res.json(404, error);
 });
 
+//Add new Record
+app.post('/accounts/records', function(req, res) {
+	var record 			= new Object();
+	record.amount 		= req.body.amount;
+	record.category 	= req.body.category;
+	record.date			= req.body.date;
+	record.description 	= req.body.description;
+	record.is_expense	= req.body.is_expense;
+	record.id 			= Math.ceil(Math.random() * 1000000); //TODO Change when integrated with MongoDB
+
+	var account_id = req.body.account_id;
+
+	for(var accountKey in accounts) {
+		if (accounts[accountKey].id == account_id) {
+			accounts[accountKey].records.push(record);
+			res.json(200, {id:record.id});
+		}
+	}
+});
+
+//Delete Record
+app.delete('/accounts/:accountId/records/:recordId', function(req, res) {
+	for (var accountKey in accounts) {
+		if (accounts[accountKey].id == req.params.accountId) {
+			var records = accounts[accountKey].records;
+			for (var recordKey in records) {
+				if (records[recordKey].id == req.params.recordId) {	
+					accounts[accountKey].records.splice(recordKey, 1);
+					res.send(200);
+				}
+			}
+		}
+	}
+	res.json(404, error);
+});
+
+
+//Get all categories
 app.get('/categories', function(req, res) {
-	res.header('Access-Control-Allow-Origin', "http://localhost");
-	
   	res.json(categories);
 });
 
--- app/js/AccountsController.js
+++ app/js/AccountsController.js
@@ -3,30 +3,60 @@ var budgetControllers = angular.module('budgetControllers', []);
 
 budgetControllers.controller('AccountListCtrl', ['$scope', '$http',
 	function AccountListCtrl($scope, $http) {
+		$scope.accounts = [];
+
 	    $http.get('http://localhost:3000/accounts').success(function(data) {
 	    	$scope.accounts = data;
 	    });
+
+
+	    $scope.addAccount = function(account) {
+	    	if (account === undefined || account.name == null || account.currency == null) {
+	    		return ;
+	    	}
+	    	//Create the new account with form input values
+	    	var a 		= new Object();
+	    	a.name 		= account.name;
+	    	a.currency 	= account.currency;
+	    	a.records 	= new Array();
+	    	a.balance 	= 0;
+
+	    	//Save Account
+	    	$http.post('http://localhost:3000/accounts', a).success(function(data) {
+	    		a.id = data.id;
+		    	$scope.accounts.push(a)
+		    });
+	    };
+
+	    $scope.deleteAccount = function(accountId) {
+	    	$http.delete('http://localhost:3000/accounts/' + accountId).success(function(data) {
+	    		var accounts = $scope.accounts;
+		    	for (var accountKey in accounts) {
+		    		if (accounts[accountKey].id == accountId) {
+		    			$scope.accounts.splice(accountKey, 1);
+		    			return ;
+		    		}
+	    		}
+		    });
+	    };
+
 	}]);
  
-budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$http',
-	function AccountDetailCtrl($scope, $routeParams, $http) {
-		$http.get('http://localhost:3000/accounts/' + $routeParams.accountId).success(function(data) {
+budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$http', '$location',
+	function AccountDetailCtrl($scope, $routeParams, $http, $location) {
+		$scope.categories 	= [];
+		$scope.account 		= {};
+		var account_id		= $routeParams.accountId;
+
+		$http.get('http://localhost:3000/accounts/' + account_id).success(function(data) {
 			$scope.account = data;
+		}).error(function(data, status) {
+			$location.path("/accounts");
 		});
 
 		$http.get('http://localhost:3000/categories').success(function(data) {
 	    	$scope.categories = data;
-	    });
-
-	    //init addRecordForm
-	    var defaultAddRecordForm = {
-	        amount: 0.0,
-	        type: 0,
-	        category: "Utilities",
-	        description: ""
-	    };
-	    
-	    $scope.record = defaultAddRecordForm
+	    })
 
 	    $scope.getBalance = function() {
 	    	var records = $scope.account.records;
@@ -43,13 +73,14 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 	    };
 
 	    $scope.addRecord = function(record) {
-	    	var amount = Number(record.amount);
-	    	if (isNaN(amount) || amount <= 0) {
-	    		$scope.record = defaultAddRecordForm;
+	    	if (record === undefined) {
 	    		return ;
 	    	}
 
-	    	$scope.addRecordForm.$setPristine();
+	    	var amount = Number(record.amount);
+	    	if (isNaN(amount) || amount <= 0) {
+	    		return ;
+	    	}
 
 	    	//Create the new record with form input values
 	    	var r 			= new Object();
@@ -58,24 +89,26 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 	    	r.date 			= new Date().getTime();
 	    	r.amount 		= amount;
 	    	r.is_expense 	= record.type == 0 ? true : false;
+	    	r.account_id	= account_id;
 
-	    	//TODO: Call API to save record
 
-	    	//Set form inputs with default values
-	    	$scope.record = defaultAddRecordForm;
-
-	    	return $scope.account.records.push(r);
+	    	//Save Record
+	    	$http.post('http://localhost:3000/accounts/records', r).success(function(data) {
+	    		r.id = data.id;
+		    	$scope.account.records.push(r)
+		    });
 	    };
 
 	    $scope.deleteRecord = function(record) {
-	    	var records = $scope.account.records;
-	    	for (var recordKey in records) {
-	    		if (records[recordKey].id == record.id) {
-	    			$scope.account.records.splice(recordKey, 1);
-	    			return ;
-	    		}
-	    	}
-	    }
+	    	$http.delete('http://localhost:3000/accounts/' + account_id + '/records/' + record.id).success(function(data) {
+		    	var records = $scope.account.records;
+		    	for (var recordKey in records) {
+		    		if (records[recordKey].id == record.id) {
+		    			return $scope.account.records.splice(recordKey, 1);
+		    		}
+		    	}
+		    });
+	    };
 
 	    function displayChart() {
 	    	var records = $scope.account.records;
@@ -97,5 +130,5 @@ budgetControllers.controller('AccountDetailCtrl', ['$scope', '$routeParams', '$h
 
 	      	//Display the data
 	    	new Chart(document.getElementById("canvas").getContext("2d")).Pie(pieData);
-	    }
+	    };
 	}]);
